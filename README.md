ba41042b7c696aff27ec547363110158a364c18a
Pich Him <pichhim@live.unc.edu>
2020-10-08 13:33:56 -0400

    Added routes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # COMP-426-Final-Project
+
+# Notes
+- Had to 'npm install react-router-dom --save' for the program to start up
--- final-project/src/App.js
+++ final-project/src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from './components/Landing';
-import TicTacToe from './components/TicTacToe';
-import ConnectFour from './components/ConnectFour';
-import Othello from './components/Othello';
+import ProfilePage from './components/Profile';
+import MessagesPage from './components/Messages';
 
 function App() {
   return (
@@ -12,11 +11,8 @@ function App() {
     <Router>
         <div className="App">
           <Route exact path={"/"} component={LandingPage}></Route>
-          <Route exact path={"/tictactoe"} component={TicTacToe}></Route>
-          <Route exact path={"/connectfour"} component={ConnectFour}></Route>
-          <Route exact path={"/othello"} component={Othello}></Route>
-          <Route exact path={"/profile"} component={TicTacToe}></Route>
-          <Route exact path={"/messages"} component={TicTacToe}></Route>
+          <Route exact path={"/profile"} component={ProfilePage}></Route>
+          <Route exact path={"/messages"} component={MessagesPage}></Route>
         </div>
     </Router>
   );
