5ad5bf9ed37db65ea37a8a42235e9a4b1b599554
Lucas Zhang <lhzhang@live.unc.edu>
2020-11-11 21:42:25 -0500

    Connected navbar to firebase

--- final-project/src/components/Navigation/index.js
+++ final-project/src/components/Navigation/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { withFirebase } from '../Firebase';
 //import { Button } from 'react-bulma-components'
 
 import SignOutButton from '../SignOut';
@@ -8,7 +9,7 @@ import Landing from '../Landing';
 
 const Navigation = ({ authUser }) => (
   // Selectively renders Nav bar based on authUser state
-  <div>{authUser ? <NavAuth /> : <NavNonAuth />}</div>
+  <div>{authUser ? <NavAuth user={authUser} /> : <NavNonAuth />}</div>
 );
 
 const imageStyle = function (length) {
@@ -63,60 +64,82 @@ const style = {
 //   </div>
 // );
 
-const NavAuth = () => (
-  // <div>
+const NavAuth = withFirebase(props => {
 
-  //   <Link to={'/'}>Landing   </Link>
-  //   <Link to={'profile'}>Profile   </Link>
-  //   <Link to={'/messages'}>Messages   </Link>
-  //   <Link to={'/demo'}>Game Board Demo   </Link>
-  //   <SignOutButton />
-  // </div>
+  // All user snapshot data has been stored into the currUser variable
+  const [currUser, setCurrUser] = useState(null);
 
-  <nav className="navbar" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <Link className="navbar-item" to="/">
-        <h1 className="title is-2">logo?</h1>
-        {/* <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" /> */}
-      </Link>
-    </div>
-    <div id="navbarBasicExample" className="navbar-menu">
-      <div className="navbar-start">
-        <Link to="/" className="navbar-item">
-          <h1 className="subtitle is-4">home</h1>
-        </Link>
-        <Link to="/messages" className="navbar-item">
-          <h1 className="subtitle is-4">messages</h1>
-        </Link>
-        <Link to="/demo" className="navbar-item">
-          <h1 className="subtitle is-4">games</h1>
+  function getUserData() {
+    const db = props.firebase.getDB();
+    let uid = props.user.uid;
+
+    try {
+      let listener = db.ref(`/users/${uid}`).on("value", snapshot => {
+        setCurrUser(snapshot.val())
+      })
+      return () => db.ref(`/users/${uid}`).off("value", listener);
+    } catch (error) {
+      alert("Error reading user channels")
+    }
+  }
+
+  useEffect(getUserData, []);
+
+  return (
+    // <div>
+
+    //   <Link to={'/'}>Landing   </Link>
+    //   <Link to={'profile'}>Profile   </Link>
+    //   <Link to={'/messages'}>Messages   </Link>
+    //   <Link to={'/demo'}>Game Board Demo   </Link>
+    //   <SignOutButton />
+    // </div>
+
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">
+          <h1 className="title is-2">logo?</h1>
+          {/* <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" /> */}
         </Link>
       </div>
-
-      <div className="navbar-end">
-      <a href="/demo" className="navbar-item">
-          <h1 className="subtitle is-5">welcome, blah</h1>
-        </a>
-        <div className="navbar-item has-dropdown is-hoverable">
-          <Link to="/profile" className="navbar-link is-arrowless">
-            <div style={style.profilePic}>
-              <img style={imageStyle(45)} src="https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png?w=640" />
-            </div>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item">
+            <h1 className="subtitle is-4">home</h1>
           </Link>
-          <div className="navbar-dropdown">
+          <Link to="/messages" className="navbar-item">
+            <h1 className="subtitle is-4">messages</h1>
+          </Link>
+          <Link to="/demo" className="navbar-item">
+            <h1 className="subtitle is-4">games</h1>
+          </Link>
+        </div>
+
+        <div className="navbar-end">
+          <a href="/demo" className="navbar-item">
+            {currUser ? <h1 className="subtitle is-5">Welcome, {currUser.fullname}</h1> : null}
+          </a>
+          <div className="navbar-item has-dropdown is-hoverable">
             <Link to="/profile" className="navbar-link is-arrowless">
-              Profile
+              <div style={style.profilePic}>
+                <img style={imageStyle(45)} src="https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png?w=640" />
+              </div>
             </Link>
-            <Link className="navbar-item" to="/">
-              <SignOutButton />
+            <div className="navbar-dropdown">
+              <Link to="/profile" className="navbar-link is-arrowless">
+                Profile
             </Link>
+              <Link className="navbar-item" to="/">
+                <SignOutButton />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
-);
+  );
+});
 
 const NavNonAuth = () => (
   <div>
