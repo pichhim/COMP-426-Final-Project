b17b6c349b74dc81ab44b86030be1f34ac076e4d
Lucas Zhang <lhzhang@live.unc.edu>
2020-11-21 11:15:21 -0500

    Updates

---
+++ precache-manifest.954b0dd5c71073ec3eeb1fa39e098c89.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4709086ed45027ff2fb0401cce4ee0",
+    "url": "/COMP-426-Final-Project/index.html"
+  },
+  {
+    "revision": "d4a4363bd8591e194682",
+    "url": "/COMP-426-Final-Project/static/css/2.ffc89db8.chunk.css"
+  },
+  {
+    "revision": "d7fe9f066b600b44463f",
+    "url": "/COMP-426-Final-Project/static/css/main.2e572941.chunk.css"
+  },
+  {
+    "revision": "d4a4363bd8591e194682",
+    "url": "/COMP-426-Final-Project/static/js/2.7b2dbf95.chunk.js"
+  },
+  {
+    "revision": "8d1e2746909a67d5ee5a24b4222b85e2",
+    "url": "/COMP-426-Final-Project/static/js/2.7b2dbf95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fe9f066b600b44463f",
+    "url": "/COMP-426-Final-Project/static/js/main.2d10f0e9.chunk.js"
+  },
+  {
+    "revision": "ec6807d11aea376d05b1",
+    "url": "/COMP-426-Final-Project/static/js/runtime-main.64160eee.js"
+  },
+  {
+    "revision": "51f8d1b45dfd9688aaf60309f23828a6",
+    "url": "/COMP-426-Final-Project/static/media/boba-squiggle.51f8d1b4.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/COMP-426-Final-Project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/COMP-426-Final-Project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/COMP-426-Final-Project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/COMP-426-Final-Project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/COMP-426-Final-Project/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "430685b1f2bcf1aa421c530b3933ee68",
+    "url": "/COMP-426-Final-Project/static/media/logo.430685b1.png"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/COMP-426-Final-Project/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/COMP-426-Final-Project/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/COMP-426-Final-Project/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/COMP-426-Final-Project/static/media/notification.c0d3c94c.eot"
+  }
+]);
